7daa743dc84ce8b09ee4a5309c7c1c6e3b15b4c4
jerem <jeremie.primas@gmail.com>
2020-04-11 18:28:10 +0200

    Ajout des boutons et gestion du scroll

--- TodoList.Application/Tools/AppUtils.cs
+++ TodoList.Application/Tools/AppUtils.cs
@@ -4,21 +4,14 @@ namespace TodoList.Application.Tools
 {
     public static class AppUtils
     {
-        public static string GetStatusTypeText(StatusTypes? statusType)
-        {
-            switch (statusType)
+        public static string GetStatusTypeText(StatusTypes? statusType) =>
+            statusType switch
             {
-                case StatusTypes.Todo:
-                    return "To do";
-                case StatusTypes.Postponed:
-                    return "Postponed";
-                case StatusTypes.Cancelled:
-                    return "Cancelled";
-                case StatusTypes.Done:
-                    return "Done";
-                default:
-                    return string.Empty;
-            }
-        }
+                StatusTypes.Todo        => "Todo",
+                StatusTypes.Postponed   => "Postponed",
+                StatusTypes.Cancelled   => "Cancelled",
+                StatusTypes.Done        => "Done",
+                _                       => string.Empty
+            };
     }
 }
