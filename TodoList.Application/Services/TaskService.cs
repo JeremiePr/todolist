f6103c4ff7ef9378e846e5c65ebfeb645879b1d6
jerem <jeremie.primas@gmail.com>
2020-04-12 02:16:02 +0200

    Affichage des différents types de task et update des statut

---
+++ TodoList.Application/Services/TaskService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TodoList.Application.Models;
+
+namespace TodoList.Application.Services
+{
+    public class TaskService
+    {
+        private readonly HttpClient _http;
+        private readonly string _baseUrl;
+
+        public TaskService(HttpClient http, AppSettings appSettings)
+        {
+            _http = http;
+            _baseUrl = appSettings.BaseApiUrl + "/Tasks";
+        }
+
+        public async Task<TaskDTO> UpdateTask(TaskDTO task)
+        {
+            return await _http.PutJsonAsync<TaskDTO>(_baseUrl, task);
+        }
+    }
+}
